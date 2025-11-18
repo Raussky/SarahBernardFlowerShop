0cc2b69ad2b85679d2bc769a18cb0d3e1684869b
Raussky <rauangadil5@gmail.com>
2025-11-18 15:13:55 +0500

    feat(android): update app orientation to default and add resizeable activity plugin for compatibility

---
+++ plugins/withAndroidResizeableActivity.js
@@ -0,0 +1,28 @@
+const { withAndroidManifest } = require('@expo/config-plugins');
+
+/**
+ * Config plugin to enable resizeable activity and remove screen orientation restrictions
+ * This fixes Android 15/16 compatibility issues with large screens
+ */
+const withAndroidResizeableActivity = (config) => {
+  return withAndroidManifest(config, async (config) => {
+    const androidManifest = config.modResults;
+    const mainApplication = androidManifest.manifest.application[0];
+
+    if (mainApplication && mainApplication.activity) {
+      mainApplication.activity.forEach((activity) => {
+        if (activity.$['android:name'] === '.MainActivity') {
+          // Remove screenOrientation attribute
+          delete activity.$['android:screenOrientation'];
+
+          // Add resizeableActivity attribute
+          activity.$['android:resizeableActivity'] = 'true';
+        }
+      });
+    }
+
+    return config;
+  });
+};
+
+module.exports = withAndroidResizeableActivity;
